531b9907ba970b8b7acf1ec1dcd2b460a758c54e
Eduardo França <gothedufranca@gmail.com>
2016-11-09 14:02:13 -0200

    Atualização da classe LeTxt
    Atualização da classe corrigindo o método.

--- GerenciamentoDeCondominio/src/br/com/cotemig/persistence/LeTxt.java
+++ GerenciamentoDeCondominio/src/br/com/cotemig/persistence/LeTxt.java
@@ -1,79 +1,32 @@
 package br.com.cotemig.persistence;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.SQLException;
+import javax.swing.JFileChooser;
 
-import javax.swing.JOptionPane;
-@SuppressWarnings("unused")
 public class LeTxt {
-	private String cnpj;
-	private String cpf;
-	private String data;
-	private double valor;
-	
-	private Connection connection = null;
-	
-	public void setValues() {
-		try {
-			String message = "Insira o nome do documento de texto que contém os dados.\n"
-					+ "O mesmo deve estar salvo no Desktop (Área de Trabalho) para poder"
-					+ " ser localizado pelo sistema.\nNão é necessário digitar a extensã"
-					+ "o do arquivo (.txt).";
-			String file = "txt"; //JOptionPane.showInputDialog(message);
-			FileInputStream stream = new FileInputStream(System.getProperty("user.home") + "\\Desktop\\" + file + ".txt");
-			InputStreamReader reader = new InputStreamReader(stream);
-			BufferedReader br = new BufferedReader(reader);
-			String linha = br.readLine();
-			if(linha.length() != 39){
-				JOptionPane.showMessageDialog(null, "Informações inválidas, favor tentar novamente.");
-			}else{
-				this.cnpj = linha.substring(0, 14);
-				this.cpf = linha.substring(14, 25);
-				this.data = linha.substring(25, 31);
-				this.valor = Double.parseDouble(linha.substring(31, 39))/100;
-				br.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}/*finally {
-			try {
-				connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}*/
-	}
-	
-	/**
-	 * @return the cnpj
-	 */
-	public String getCnpj() {
-		return cnpj;
-	}
-	
-	/**
-	 * @return the cpf
-	 */
-	public String getCpf() {
-		return cpf;
+
+	public static String openFile() {
+		JFileChooser jfc = new JFileChooser();
+		int dialog = jfc.showOpenDialog(null);
+		String path = null;
+		if (dialog == JFileChooser.APPROVE_OPTION) {
+			path = jfc.getSelectedFile().getAbsolutePath();
+		}
+		return path;
 	}
 
-	/**
-	 * @return the data
-	 */
-	public String getData() {
-		return data;
+	public static String readFile(String path) {
+		String linha = null;
+		try {
+			FileReader arq = new FileReader(path);
+			BufferedReader lerArq = new BufferedReader(arq);
+			linha = lerArq.readLine();
+			arq.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return linha;
 	}
-	
-	/**
-	 * @return the valor
-	 */
-	public double getValor() {
-		return valor;
-	}
-	
 }
