1c5034c8d8635de753c35c4055cee738b2883867
Eduardo França <gothedufranca@gmail.com>
2016-11-09 17:40:56 -0200

    Changed Main class
    Changed in order to test LeTxt class.

--- GerenciamentoDeCondominio/src/br/com/cotemig/main/Main.java
+++ GerenciamentoDeCondominio/src/br/com/cotemig/main/Main.java
@@ -1,17 +1,13 @@
 package br.com.cotemig.main;
 
 import br.com.cotemig.persistence.LeTxt;
-import br.com.cotemig.persistence.UseDataBase;
 
 public class Main {
-
 	public static void main(String[] args) {
-		LeTxt dados = new LeTxt();
-		dados.setValues();
-		boolean x = UseDataBase.insereConta(dados.getCnpj(), dados.getCpf(), dados.getData(), dados.getValor());
-		if (x)
-			System.out.println("Ok");
+		String path = "123456789012341234567890112345612345678";
+		if(LeTxt.readFile(LeTxt.openFile()).equals(path))
+			System.out.println("ok");
 		else
-			System.out.println("Bad");
+			System.out.println("bad");
 	}
 }
